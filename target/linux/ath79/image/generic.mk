12445ae9dd95d49750b951735c4a119c083c2fcd
Sebastian Schaper <openwrt@sebastianschaper.net>
2020-10-07 19:56:19 +0200

    ath79: add support for Zyxel NWA5123-NI
    Specifications:
     * AR9344, 8 MiB Flash, 64 MiB RAM
    
    Installation:
     * enter bootloader via serial console, change bootcmd to
         bootm 0x9f050000
     * rename image to mi124_f1e-jffs2, provide via tftp at 192.168.1.10, call
         run lf

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -152,7 +152,6 @@ define Build/wrgg-pad-rootfs
 	$(STAGING_DIR_HOST)/bin/padjffs2 $(IMAGE_ROOTFS) -c 64 >>$@
 endef
 
-
 define Device/seama
   KERNEL := kernel-bin | append-dtb | relocate-kernel | lzma
   KERNEL_INITRAMFS := $$(KERNEL) | seama
@@ -1692,6 +1691,16 @@ define Device/zbtlink_zbt-wd323
 	kmod-usb-serial kmod-usb-serial-cp210x uqmi
 endef
 TARGET_DEVICES += zbtlink_zbt-wd323
+
+define Device/zyxel_nwa5123-ni
+  SOC := ar9342
+  DEVICE_VENDOR := Zyxel
+  DEVICE_MODEL := NWA5123
+  DEVICE_VARIANT := NI
+  DEVICE_PACKAGES := kmod-ath9k
+  IMAGE_SIZE := 15680k
+endef
+TARGET_DEVICES += zyxel_nwa5123-ni
 
 define Device/zyxel_nbg6616
   SOC := qca9557
