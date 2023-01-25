1b26e5b453a5d08791aae3fbd780dc5f8b05e8ad
Sebastian Schaper <openwrt@sebastianschaper.net>
2023-01-25 15:47:59 +0100

    ath79: add WIP support for D-Link DSP-W215 A1
    broken as of now:
    - does not build due to image size

--- target/linux/ath79/tiny/base-files/etc/uci-defaults/50_wifi_defaults.sh
+++ target/linux/ath79/tiny/base-files/etc/uci-defaults/50_wifi_defaults.sh
@@ -4,6 +4,15 @@
 board=$(board_name)
 
 case "$board" in
+dlink,dsp-w215-a1)
+	ssid=$(cat /sys/bus/nvmem/devices/mtd$(find_mtd_index 'mac')/cells/label-ssid@2d,0)
+	pin=$(cat /sys/bus/nvmem/devices/mtd$(find_mtd_index 'mac')/cells/label-pin@4e,0)
+	uci set wireless.@wifi-device[0].disabled=0
+	uci set wireless.@wifi-iface[0].ssid="$ssid"
+	uci set wireless.@wifi-iface[0].key="$ssid$pin"
+	uci set wireless.@wifi-iface[0].encryption="psk2+aes"
+	uci commit wireless
+	;;
 dlink,dsp-w215-b1)
 	ssid=$(cat /sys/bus/nvmem/devices/mtd$(find_mtd_index 'mp')/cells/label-ssid@4e,0)
 	pin=$(cat /sys/bus/nvmem/devices/mtd$(find_mtd_index 'mp')/cells/label-pin@3e,0)
