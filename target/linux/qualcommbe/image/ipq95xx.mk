a042a9912bdae40403be7ffd915f673aaa68efca
Sebastian Schaper <openwrt@sebastianschaper.net>
2025-09-13 17:03:59 +0200

    add image size

--- target/linux/qualcommbe/image/ipq95xx.mk
+++ target/linux/qualcommbe/image/ipq95xx.mk
@@ -34,6 +34,7 @@ define Device/tplink_archer-ge800-v1
 	DEVICE_DTS_CONFIG := config@ge800
 	SOC := ipq9574
 	DEVICE_PACKAGES := kmod-ath12k ath12k-firmware-qcn9274 ipq-wifi-8devices_kiwi f2fsck mkf2fs kmod-sfp kmod-phy-aquantia
+	IMAGE_SIZE := 25344k
 	IMAGE/sysupgrade.bin := append-kernel | pad-to 64k | append-rootfs | pad-rootfs | check-size | append-metadata
 endef
 TARGET_DEVICES += tplink_archer-ge800-v1
